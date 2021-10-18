a75ca0744c865958f3a613525553d04e80919c88
Jonathan Colon <jcolonf@zenprsolutions.com>
2021-10-17 20:41:18 -0400

    Added Protected, Recovery Site Information to Main Report

--- Src/Public/Invoke-AsBuiltReport.VMware.SRM.ps1
+++ Src/Public/Invoke-AsBuiltReport.VMware.SRM.ps1
@@ -7,19 +7,21 @@ function Invoke-AsBuiltReport.VMware.SRM {
     .NOTES
         Version:        0.1.0
         Author:         Jonathan Colon
-        Twitter:        
-        Github:         
+        Twitter:        @jcolonfzenpr
+        Github:         @rebelinux
         Credits:        Iain Brighton (@iainbrighton) - PScribo module
 
     .LINK
         https://github.com/AsBuiltReport/AsBuiltReport.VMware.SRM
     #>
-	
+
 	# Do not remove or add to these parameters
     param (
         [String[]] $Target,
         [PSCredential] $Credential
     )
+    # Check if the required version of VMware PowerCLI is installed
+    Get-RequiredModule -Name 'VMware.PowerCLI' -Version '12.3'
 
     # Import Report Configuration
     $Report = $ReportConfig.Report
@@ -30,12 +32,28 @@ function Invoke-AsBuiltReport.VMware.SRM {
     $TextInfo = (Get-Culture).TextInfo
 
 	# Update/rename the $System variable and build out your code within the ForEach loop. The ForEach loop enables AsBuiltReport to generate an as built configuration against multiple defined targets.
-	
+
     #region foreach loop
-    foreach ($System in $Target) {
-		
-		
-		
+    #---------------------------------------------------------------------------------------------#
+    #                                 Connection Section                                          #
+    #---------------------------------------------------------------------------------------------#
+    foreach ($VIServer in $Target) {
+        try {
+            $vCenter = Connect-VIServer $VIServer -Credential $Credential -ErrorAction Stop
+            $SRMServer = Connect-SrmServer -IgnoreCertificateErrors -ErrorAction Stop -Port 443 -Protocol https -Credential $Credential -RemoteCredential $Credential
+        } catch {
+            Write-Error $_
+        }
+
+        if ($SRMServer) {
+            Section -Style Heading1 "VMware Site Recovery Manager - $($SRMServer.Name.split(".", 2).toUpper()[0])." {
+                Paragraph "VMware Site Recovery Manager is a business continuity and disaster recovery solution that helps you plan, test, and run the recovery of virtual machines between a protected vCenter Server site and a recovery vCenter Server site. You can use Site Recovery Manager to implement different types of recovery from the protected site to the recovery site."
+                BlankLine
+                Get-AbrSRMProtectedSiteInfo
+                Get-AbrSRMRecoverySiteInfo
+            }
+
+        }
 	}
 	#endregion foreach loop
 }
