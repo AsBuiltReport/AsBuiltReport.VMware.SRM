c8e9ef7217dcc0865671b387dda990065043e59c
Jonathan Colon <jcolonf@zenprsolutions.com>
2021-10-21 22:19:16 -0400

    Added Folder Mapping to Summary Section

--- Src/Private/Get-AbrSRMSummaryInfo.ps1
+++ Src/Private/Get-AbrSRMSummaryInfo.ps1
@@ -65,6 +65,44 @@ function Get-AbrSRMSummaryInfo {
         catch {
             Write-PscriboMessage -IsWarning $_.Exception.Message
         }
+        try {
+            $FolderMapping = $SRMServer.ExtensionData.InventoryMapping.GetFolderMappings()
+            Section -Style Heading2 'Folder Mapping Summary' {
+                Paragraph "The following section provides a summary of the Folder Mapping on Site $($SRMServer.ExtensionData.GetLocalSiteInfo().SiteName)."
+                BlankLine
+                $OutObj = @()
+                $LocalSitevCenter = (Get-AdvancedSetting -Entity $LocalvCenter | Where-Object {$_.name -eq 'VirtualCenter.FQDN'}).Value
+                $RemoteSitevCenter = (Get-AdvancedSetting -Entity $RemotevCenter | Where-Object {$_.name -eq 'VirtualCenter.FQDN'}).Value
+                if ($FolderMapping) {
+                    $FolderHash = $Null
+                    foreach ($FolderMap in $FolderMapping) {
+                        Write-PscriboMessage "Discovered Folder Mapping information for $($SRMServer.ExtensionData.GetLocalSiteInfo().SiteName)."
+                        $LocalFolder = get-view $FolderMap.PrimaryObject | Select-Object -ExpandProperty Name
+                        $RemoteFolder = get-view $FolderMap.SecondaryObject | Select-Object -ExpandProperty Name
+                        $FolderHash = @{
+                            $LocalFolder = $RemoteFolder
+                        }
+                        $inObj = [ordered] @{
+                            "Local Folder" = $FolderHash.Keys
+                            "Remote Folder" = $FolderHash.Values
+                        }
+                        $OutObj += [pscustomobject]$inobj
+                    }
+                }
+                $TableParams = @{
+                    Name = "Folder Mapping - $($SRMServer.ExtensionData.GetLocalSiteInfo().SiteName)"
+                    List = $false
+                    ColumnWidths = 50, 50
+                }
+                if ($Report.ShowTableCaptions) {
+                    $TableParams['Caption'] = "- $($TableParams.Name)"
+                }
+                $OutObj | Table @TableParams
+            }
+        }
+        catch {
+            Write-PscriboMessage -IsWarning $_.Exception.Message
+        }#>
     }
     end {}
 }
