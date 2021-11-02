7d97ac470d25f95115583fafa88f040ac841adbd
Jonathan Colon <jcolonf@zenprsolutions.com>
2021-11-02 08:22:53 -0400

    added proccess block

--- Src/Private/Get-RequiredModule.ps1
+++ Src/Private/Get-RequiredModule.ps1
@@ -9,26 +9,30 @@ function Get-RequiredModule {
     .PARAMETER Version
     The version of the required PowerShell module
     #>
-    [CmdletBinding()]
 
     Param
     (
+        [CmdletBinding()]
         [Parameter(Mandatory = $true, ValueFromPipeline = $false)]
         [ValidateNotNullOrEmpty()]
-        [String]$Name,
+        [String]
+        $Name,
 
         [Parameter(Mandatory = $true, ValueFromPipeline = $false)]
         [ValidateNotNullOrEmpty()]
-        [String]$Version
+        [String]
+        $Version
     )
 
-    # Check if the required version of VMware PowerCLI is installed
-    $RequiredModule = Get-Module -ListAvailable -Name $Name | Sort-Object -Property Version -Descending | Select-Object -First 1
-    $ModuleVersion = "$($RequiredModule.Version.Major)" + "." + "$($RequiredModule.Version.Minor)"
-    if ($ModuleVersion -eq ".")  {
-        throw "VMware PowerCLI $Version or higher is required to run the VMware vSphere As Built Report. Run 'Install-Module -Name $Name -MinimumVersion $Version' to install the required modules."
-    }
-    if ($ModuleVersion -lt $Version) {
-        throw "VMware PowerCLI $Version or higher is required to run the VMware vSphere As Built Report. Run 'Update-Module -Name $Name -MinimumVersion $Version' to update the required modules."
+    process {
+        # Check if the required version of VMware PowerCLI is installed
+        $RequiredModule = Get-Module -ListAvailable -Name $Name | Sort-Object -Property Version -Descending | Select-Object -First 1
+        $ModuleVersion = "$($RequiredModule.Version.Major)" + "." + "$($RequiredModule.Version.Minor)"
+        if ($ModuleVersion -eq ".")  {
+            throw "VMware PowerCLI $Version or higher is required to run the VMware vSphere As Built Report. Run 'Install-Module -Name $Name -MinimumVersion $Version' to install the required modules."
+        }
+        if ($ModuleVersion -lt $Version) {
+            throw "VMware PowerCLI $Version or higher is required to run the VMware vSphere As Built Report. Run 'Update-Module -Name $Name -MinimumVersion $Version' to update the required modules."
+        }
     }
 }
