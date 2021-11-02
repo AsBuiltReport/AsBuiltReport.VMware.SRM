a1cd1e752c0f0b12fa3893c1b4fc34b19794ac85
Jonathan Colon <jcolonf@zenprsolutions.com>
2021-11-01 21:49:30 -0400

    Completed v0.2.0 release

--- Src/Public/Invoke-AsBuiltReport.VMware.SRM.ps1
+++ Src/Public/Invoke-AsBuiltReport.VMware.SRM.ps1
@@ -5,7 +5,7 @@ function Invoke-AsBuiltReport.VMware.SRM {
     .DESCRIPTION
         Documents the configuration of VMware SRM in Word/HTML/Text formats using PScribo.
     .NOTES
-        Version:        0.1.0
+        Version:        0.2.0
         Author:         Jonathan Colon
         Twitter:        @jcolonfzenpr
         Github:         @rebelinux
