1b79efc1bc53d4d2c4a6cb12744e56405f8a8cfa
Jonathan Colon <jcolonf@zenprsolutions.com>
2021-12-11 19:17:47 -0400

    Added SRM Server VM Properties information

--- Src/Private/Get-AbrSRMRecoverySiteInfo.ps1
+++ Src/Private/Get-AbrSRMRecoverySiteInfo.ps1
@@ -35,8 +35,14 @@ function Get-AbrSRMRecoverySiteInfo {
                 BlankLine
                 $OutObj = @()
                 if ($RecoverySiteInfo) {
+                    $extensionmanager = get-view extensionmanager -Server $RemotevCenter
+                    $extension = $extensionmanager.extensionlist | where-object { $_.key -eq "com.vmware.vcDR" }
+                    if($extension.count -eq 1){
+	                    $RemoteSRM = $extension.server.url.split("/")[2].split(":")[0]
+                    } else {$RemoteSRM = "Unknown"}
                     Write-PscriboMessage "Discovered Recovery Site $($RecoverySiteInfo.Name)."
                     $inObj = [ordered] @{
+                        'Recovery Server Name' = $RemoteSRM
                         'Recovery Site Name' = $RecoverySiteInfo.Name
                         'Recovery Site ID' = $RecoverySiteInfo.Uuid
                         'Solution User' = $LocalSRM.ExtensionData.GetPairedSiteSolutionUserInfo().Username
@@ -61,6 +67,54 @@ function Get-AbrSRMRecoverySiteInfo {
                     $TableParams['Caption'] = "- $($TableParams.Name)"
                 }
                 $OutObj | Table @TableParams
+                try {
+                    $extensionmanager = get-view extensionmanager -Server $RemotevCenter
+                    $extension = $extensionmanager.extensionlist | where-object { $_.key -eq "com.vmware.vcDR" }
+                    if($extension.count -eq 1){
+	                    $RemoteSRM = $extension.server.url.split("/")[2].split(":")[0]
+                    }
+                    $RemoteSRMFQDM = $RemoteSRM
+                    $RemoteSRMHostName = $RemoteSRMFQDM.Split(".")[0]
+                    if ($RemoteSRMFQDM) {
+                        $RemoteSRMVM = Get-VM * | where-object {$_.Guest.HostName -match $RemoteSRMFQDM}
+                    }
+                    elseif (!$RemoteSRMVM) {
+                        $RemoteSRMVM = Get-VM * | where-object {$_.Guest.VmName -match $RemoteSRMHostName}
+                    }
+                    if ($RemoteSRMVM) {
+                        Section -Style Heading4 "SRM Server VM Properties" {
+                            Paragraph "The following section provides the hardware properties of the Protected Site $($RecoverySiteInfo.Name)."
+                            BlankLine
+                            $OutObj = @()
+                            Write-PscriboMessage "Discovered SRM VM Properties $($RemoteSRMVM.Name)."
+                            $inObj = [ordered] @{
+                                'VM Name' = $RemoteSRMVM.Name
+                                'Number of CPUs' = $RemoteSRMVM.NumCpu
+                                'Cores Per Socket' = $RemoteSRMVM.CoresPerSocket
+                                'Memory in GB' = $RemoteSRMVM.MemoryGB
+                                'Host' = $RemoteSRMVM.VMHost
+                                'Guest Id' = $RemoteSRMVM.GuestId
+                                'Provisioned Space GB' = "$([math]::Round(($RemoteSRMVM.ProvisionedSpaceGB)))"
+                                'Used Space GB' = "$([math]::Round(($RemoteSRMVM.UsedSpaceGB)))"
+                                'Datastores' = $RemoteSRMVM.DatastoreIdList | ForEach-Object {get-view $_ | Select-Object -ExpandProperty Name}
+                            }
+                            $OutObj += [pscustomobject]$inobj
+
+                            $TableParams = @{
+                                Name = "SRM VM Properties - $($RemoteSRMVM.Name)"
+                                List = $true
+                                ColumnWidths = 40, 60
+                            }
+                            if ($Report.ShowTableCaptions) {
+                                $TableParams['Caption'] = "- $($TableParams.Name)"
+                            }
+                            $OutObj | Table @TableParams
+                        }
+                    }
+                }
+                catch {
+                    Write-PscriboMessage -IsWarning $_.Exception.Message
+                }
             }
         }
         catch {
